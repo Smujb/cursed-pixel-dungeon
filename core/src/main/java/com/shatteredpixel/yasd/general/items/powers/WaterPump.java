9743b5816c247fbea6d4e4cc28baf4f911535212
samue <smujamesb@gmail.com>
2020-05-14 14:59:18 +0100

    Fixed water pump and added spells to map.

--- core/src/main/java/com/shatteredpixel/yasd/general/items/powers/WaterPump.java
+++ core/src/main/java/com/shatteredpixel/yasd/general/items/powers/WaterPump.java
@@ -2,8 +2,9 @@ package com.shatteredpixel.yasd.general.items.powers;
 
 import com.shatteredpixel.yasd.general.Dungeon;
 import com.shatteredpixel.yasd.general.Element;
+import com.shatteredpixel.yasd.general.actors.Actor;
 import com.shatteredpixel.yasd.general.actors.Char;
-import com.shatteredpixel.yasd.general.actors.mobs.Mob;
+import com.shatteredpixel.yasd.general.effects.CellEmitter;
 import com.shatteredpixel.yasd.general.levels.SewerLevel;
 import com.shatteredpixel.yasd.general.mechanics.Ballistica;
 import com.shatteredpixel.yasd.general.sprites.ItemSpriteSheet;
@@ -24,10 +25,13 @@ public class WaterPump extends Power {
 		int pos = shot.collisionPos;
 		boolean[] passable = Dungeon.level.passable();
 		PathFinder.buildDistanceMap(pos, passable, 3);
-		for (Mob mob : Dungeon.level.mobs.toArray(new Mob[0])) {
-			if (PathFinder.distance[mob.pos] < Integer.MAX_VALUE) {
-				mob.damage(Random.IntRange(4 + Dungeon.getScaleFactor() * 2, 10 + Dungeon.getScaleFactor() * 6), new Char.DamageSrc(Element.WATER, this));
-				mob.sprite.emitter().burst(SewerLevel.WaterParticle.FACTORY, 30);
+		for (int i = 0; i < PathFinder.distance.length; i++) {
+			if (PathFinder.distance[i] < Integer.MAX_VALUE) {
+				Char ch = Actor.findChar(i);
+				if (ch != null && ch.alignment == Char.Alignment.ENEMY) {
+					ch.damage(Random.IntRange(4 + Dungeon.getScaleFactor() * 2, 10 + Dungeon.getScaleFactor() * 6), new Char.DamageSrc(Element.WATER, this));
+				}
+				CellEmitter.get(i).burst(SewerLevel.WaterParticle.FACTORY, 10);
 			}
 		}
 		Camera.main.shake(3, 1f);
