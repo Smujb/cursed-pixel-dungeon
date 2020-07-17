416bce9a1542d2da5ddf2a63ada5edf7475a5d00
samue <smujamesb@gmail.com>
2020-07-17 16:04:53 +0100

    V0.3.9

--- core/src/main/java/com/shatteredpixel/yasd/general/scenes/WelcomeScene.java
+++ core/src/main/java/com/shatteredpixel/yasd/general/scenes/WelcomeScene.java
@@ -46,7 +46,7 @@ import com.watabou.utils.FileUtils;
 
 public class WelcomeScene extends PixelScene {
 
-	private static int LATEST_UPDATE = CPDGame.v0_3_8;
+	private static int LATEST_UPDATE = CPDGame.v0_3_9;
 
 	@Override
 	public void create() {
