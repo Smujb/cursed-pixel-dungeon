3b677610c09f42a7be1e7f4a3c69e85a5041d799
samue <smujamesb@gmail.com>
2020-08-05 17:47:01 +0100

    Porting v0.8.1: coded in chrome for silver windows

--- core/src/main/java/com/shatteredpixel/yasd/general/Chrome.java
+++ core/src/main/java/com/shatteredpixel/yasd/general/Chrome.java
@@ -35,6 +35,7 @@ public class Chrome {
 		TOAST,
 		TOAST_TR,
 		WINDOW,
+		WINDOW_SILVER,
 		RED_BUTTON,
 		GREY_BUTTON,
 		GREY_BUTTON_TR,
@@ -49,32 +50,34 @@ public class Chrome {
 	public static NinePatch get( Type type ) {
 		String Asset = Assets.Interfaces.CHROME;
 		switch (type) {
-		case WINDOW:
-			return new NinePatch( Asset, 0, 0, 20, 20, 6 );
-		case TOAST:
-			return new NinePatch( Asset, 22, 0, 18, 18, 5 );
-		case TOAST_TR:
-			return new NinePatch( Asset, 40, 0, 18, 18, 5 );
-		case RED_BUTTON:
-			return new NinePatch( Asset, 58, 0, 6, 6, 2 );
-		case GREY_BUTTON:
-			return new NinePatch( Asset, 58, 6, 6, 6, 2 );
-		case TAG:
-			return new NinePatch( Asset, 22, 18, 16, 14, 3 );
-		case GEM:
-			return new NinePatch( Asset, 0, 32, 32, 32, 13 );
-		case GREY_BUTTON_TR:
-			return new NinePatch( Asset, 53, 20, 9, 9, 5 );
-		case SCROLL:
-			return new NinePatch( Asset, 32, 32, 32, 32, 5, 11, 5, 11 );
-		case TAB_SET:
-			return new NinePatch( Asset, 64, 0, 20, 20, 6 );
-		case TAB_SELECTED:
-			return new NinePatch( Asset, 65, 22, 8, 13, 3, 7, 3, 5 );
-		case TAB_UNSELECTED:
-			return new NinePatch( Asset, 75, 22, 8, 13, 3, 7, 3, 5 );
-		default:
-			return null;
+			case WINDOW:
+				return new NinePatch(Asset, 0, 0, 20, 20, 6);
+			case WINDOW_SILVER:
+				return new NinePatch(Asset, 86, 0, 22, 22, 7);
+			case TOAST:
+				return new NinePatch(Asset, 22, 0, 18, 18, 5);
+			case TOAST_TR:
+				return new NinePatch(Asset, 40, 0, 18, 18, 5);
+			case RED_BUTTON:
+				return new NinePatch(Asset, 58, 0, 6, 6, 2);
+			case GREY_BUTTON:
+				return new NinePatch(Asset, 58, 6, 6, 6, 2);
+			case TAG:
+				return new NinePatch(Asset, 22, 18, 16, 14, 3);
+			case GEM:
+				return new NinePatch(Asset, 0, 32, 32, 32, 13);
+			case GREY_BUTTON_TR:
+				return new NinePatch(Asset, 53, 20, 9, 9, 5);
+			case SCROLL:
+				return new NinePatch(Asset, 32, 32, 32, 32, 5, 11, 5, 11);
+			case TAB_SET:
+				return new NinePatch(Asset, 64, 0, 20, 20, 6);
+			case TAB_SELECTED:
+				return new NinePatch(Asset, 65, 22, 8, 13, 3, 7, 3, 5);
+			case TAB_UNSELECTED:
+				return new NinePatch(Asset, 75, 22, 8, 13, 3, 7, 3, 5);
+			default:
+				return null;
 		}
 	}
 }
