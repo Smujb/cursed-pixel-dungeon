4c709a799dd1f6447c7c2ce51a22a1de718cd668
smuja <smujamesb@gmail.com>
2020-08-22 14:37:13 +0100

    Fixed wrong armour sprites.

--- core/src/main/java/com/shatteredpixel/yasd/general/sprites/ItemSpriteSheet.java
+++ core/src/main/java/com/shatteredpixel/yasd/general/sprites/ItemSpriteSheet.java
@@ -237,7 +237,7 @@ public class ItemSpriteSheet {
 
 	public static class Armors {
 
-		private static final int ARMOR = xy(1, 45);   //8 slots
+		private static final int ARMOR = xy(1, 46);   //8 slots
 
 		public static final int CLOTH             = ARMOR+0;
 		public static final int LEATHER           = ARMOR+1;
