0b56173cc92bb664de7c352a706f057b9467f1d9
Samuel Braithwaite <smujamesb@gmail.com>
2020-10-07 19:57:52 +0100

    Runic Shield now scales Focus/Resilience.

--- core/src/main/java/com/shatteredpixel/yasd/general/items/shield/RunicShield.java
+++ core/src/main/java/com/shatteredpixel/yasd/general/items/shield/RunicShield.java
@@ -1,5 +1,6 @@
 package com.shatteredpixel.yasd.general.items.shield;
 
+import com.shatteredpixel.yasd.general.actors.hero.Hero;
 import com.shatteredpixel.yasd.general.sprites.ItemSpriteSheet;
 
 public class RunicShield extends Shield {
@@ -7,6 +8,8 @@ public class RunicShield extends Shield {
         image = ItemSpriteSheet.Shields.RUNIC;
 
         defenseMultiplier = 0.7f;
+
+        statScaling.add(Hero.HeroStat.FOCUS);
     }
 
     @Override
