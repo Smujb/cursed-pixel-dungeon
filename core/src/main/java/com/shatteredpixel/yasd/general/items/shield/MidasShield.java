8ead8a5a0422098547a78dd960e2d9d10d298f57
Samuel Braithwaite <smujamesb@gmail.com>
2020-10-08 09:03:33 +0100

    MidasShield now actually works.

--- core/src/main/java/com/shatteredpixel/yasd/general/items/shield/MidasShield.java
+++ core/src/main/java/com/shatteredpixel/yasd/general/items/shield/MidasShield.java
@@ -2,6 +2,7 @@ package com.shatteredpixel.yasd.general.items.shield;
 
 import com.shatteredpixel.yasd.general.Dungeon;
 import com.shatteredpixel.yasd.general.actors.Char;
+import com.shatteredpixel.yasd.general.actors.buffs.Buff;
 import com.shatteredpixel.yasd.general.actors.hero.Hero;
 import com.shatteredpixel.yasd.general.items.Gold;
 import com.shatteredpixel.yasd.general.sprites.ItemSpriteSheet;
@@ -17,7 +18,24 @@ public class MidasShield extends Shield {
 
     @Override
     public int proc(Char attacker, Char enemy, int damage, boolean parry) {
-        if (!enemy.isAlive()) Dungeon.level.drop(new Gold().random(4), enemy.pos).sprite.drop();
+        new Buff() {
+            {
+                actPriority = VFX_PRIO;
+            }
+
+            @Override
+            public boolean act() {
+                spend(1f);
+                detach();
+                return true;
+            }
+
+            @Override
+            public void detach() {
+                super.detach();
+                if (!target.isAlive()) Dungeon.level.drop(new Gold().random(4), target.pos).sprite.drop();
+            }
+        }.attachTo(enemy);
         return super.proc(attacker, enemy, damage, parry);
     }
 }
