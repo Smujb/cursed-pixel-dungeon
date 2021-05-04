8a379e58d31994609a0c4a06b4ef6c87383458f9
Samuel Braithwaite <smujamesb@gmail.com>
2021-05-04 18:37:46 +0100

    Mob damage nerfed slightly to account for scaling buff.

--- core/src/main/java/com/shatteredpixel/yasd/general/actors/mobs/Mob.java
+++ core/src/main/java/com/shatteredpixel/yasd/general/actors/mobs/Mob.java
@@ -237,11 +237,11 @@ public abstract class Mob extends Char {
 	}
 
 	protected static int normalMax(int level) {
-		return Math.round(Item.calcMobPower(level) * 35);
+		return Math.round(Item.calcMobPower(level) * 30);
 	}
 
 	protected static int normalMin(int level) {
-		return Math.round(Item.calcMobPower(level) * 20);
+		return Math.round(Item.calcMobPower(level) * 15);
 	}
 
 	private static final float MAX_PARRY_CHARGE = 100f;
